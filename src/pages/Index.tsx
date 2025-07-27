417da249ddf919453a9d298821842ffbb42a55a1
user4iro <ventas1agrosuministros@gmail.com>
2025-07-27 11:51:25 -0500

    Updated Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -208,7 +208,7 @@ export default function Index() {
           <div className="absolute bottom-2 right-10 animate-pulse opacity-10">
             <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-white">
               <circle cx="12" cy="12" r="10"/>
-              <path d="M16 16s-1.5-2-4-2-4 2-4 2"/>
+              <path d="M8 14c1.5 2 6.5 2 8 0"/>
               <line x1="9" y1="9" x2="9.01" y2="9"/>
               <line x1="15" y1="9" x2="15.01" y2="9"/>
             </svg>
